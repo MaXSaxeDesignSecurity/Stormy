87e1cc4c7be12ce0758ef6dbdee33e4e9b845366
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-11-01 09:11:47 -0500

    Bootstrapping the UI
    We’ve got some of the basic building blocks in place, so let’s start displaying information in our view.

--- Stormy/CurrentWeather.swift
+++ Stormy/CurrentWeather.swift
@@ -16,8 +16,10 @@ struct CurrentWeather {
     
     init(weatherDictionary: [String:AnyObject]) {
         temperature = weatherDictionary["temperature"] as! Int
-        humidity = weatherDictionary["humidity"] as! Int
-        precipProbability = weatherDictionary["precipProbability"] as! Int
+        let humidityFloat = weatherDictionary["humidity"] as! Double
+        humidity = Int(humidityFloat * 100)
+        let percipFloat = weatherDictionary["precipProbability"] as! Double
+        precipProbability = Int(percipFloat * 100)
         summary = weatherDictionary["summary"] as! String
     }
 }
--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var currentTemperatureLabel: UILabel?
+    @IBOutlet weak var currentHumidityLabel: UILabel?
+    @IBOutlet weak var currentPrecipitationLabel: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
         let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
         let currentWeatherDictionary = weatherDictionary["currently"] as? [String:AnyObject] {
             let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
-            print(currentWeather)
+            
+            currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
+            currentHumidityLabel?.text = "\(currentWeather.humidity)%"
+            currentPrecipitationLabel?.text = "\(currentWeather.precipProbability)%"
         }
     }
 
