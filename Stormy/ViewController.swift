4697134ec1f1edb07257810bbd1645d8d70d360a
Kevin Kirsche <Kev.Kirsche@gmail.com>
2015-10-31 22:19:35 -0400

    NSBundle and the File Directory
    We have a plist and we have a struct that’s built to use data from the plist but no way to connect the two. In this video we look at how we can use the NSBundle class to load in files stored in the app’s directory.

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
+        let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
+        let currentWeatherDictionary = weatherDictionary["currently"] as? [String:AnyObject] {
+            let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
+            print(currentWeather)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +27,3 @@ class ViewController: UIViewController {
 
 
 }
-
