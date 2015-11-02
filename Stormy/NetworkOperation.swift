002f0d156de63720d70d402f6bc6a1fe89b10846
Kevin Kirsche <kev.kirsche@gmail.com>
2015-11-02 11:49:47 -0500

    Methods with Closures
    The main job of the NetworkOperation class will be to download some JSON from a given URL. Since a network operation is an asynchronous one, we need to implement our method with a callback mechanism using a closure.

--- Stormy/NetworkOperation.swift
+++ Stormy/NetworkOperation.swift
@@ -11,9 +11,42 @@ import Foundation
 class NetworkOperation {
     lazy var config: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
     lazy var session: NSURLSession = NSURLSession(configuration: self.config)
-    let queryURL = NSURL
+    let queryURL: NSURL
+
+    typealias JSONDictionaryCompletion = ([String:AnyObject]?) -> Void
 
     init(url: NSURL) {
         self.queryURL = url
+    }
+
+    func downloadJSONFromURL(completion: JSONDictionaryCompletion) {
+        let request = NSURLRequest(URL: queryURL)
+        let dataTask = session.dataTaskWithRequest(request) {
+            (let data, let response, let error) in
+
+            // 1. Check HTTP response for successful GET request
+            if let httpResponse = response as? NSHTTPURLResponse {
+                switch(httpResponse.statusCode) {
+                case 200:
+                    // 2. Create JSON object with data
+                    do {
+                        let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as! [String:AnyObject]
+                        completion(jsonDictionary)
+                    }
+                    catch {
+                        print(error)
+                    }
+                default:
+                    print("Received HTTP Status Code: \(httpResponse.statusCode)")
+                }
+            }
+            else {
+                print("Not a valid HTTP response.")
+            }
+
+
+        }
+        
+        dataTask.resume()
     }
 }
